089a6f6a7443758ed2f3dac54b5ad1e899e5b840
berryberry189 <berryberry189@naver.com>
2022-12-07 20:41:37 +0900

    CorsConfig

---
+++ src/main/kotlin/com/poc/config/CorsConfig.kt
@@ -0,0 +1,23 @@
+package com.poc.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfig : WebMvcConfigurer {
+
+    override
+    fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://138.2.117.65/")
+            .allowedHeaders("*")
+            .allowedMethods(
+                HttpMethod.GET.name,
+                HttpMethod.POST.name,
+                HttpMethod.PUT.name,
+                HttpMethod.DELETE.name
+            )
+    }
+}
